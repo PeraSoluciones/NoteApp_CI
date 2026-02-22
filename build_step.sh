9ec55e686dfa97a02b9678c9e9c9c0c8e749991e
Pablo Aucapiña <soluciones.pera@gmail.com>
2026-02-22 18:57:47 -0500

    fix: Build step file

--- build_step.sh
+++ build_step.sh
@@ -3,5 +3,5 @@
 echo "Build script"
 
 # add the commands here
-cd ./backend && rm -rf dist && cd ./frontend && npm install && npm run build && cp -r dist ../backend
-cd ./backend && npm install && npm run build
+cd backend && rm -rf dist && cd ../frontend && npm install && npm run build && cp -r dist ../backend
+cd backend && npm install && npm run build
