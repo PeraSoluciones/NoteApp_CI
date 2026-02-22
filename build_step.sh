6baedd1ddaf9a2d100334f0517b8c526c9774dde
Pablo Aucapiña <soluciones.pera@gmail.com>
2026-02-22 18:08:51 -0500

    ci: Agrega buil step para deploy

---
+++ build_step.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Build script"
+
+# add the commands here
+cd ./backend && rm -rf dist && cd ./frontend && npm install && npm run build && cp -r dist ../backend
+cd ./backend && npm install && npm run build
