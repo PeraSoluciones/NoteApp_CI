13835300334161d66e3159d96dff6911e3438cf1
Pablo Aucapiña <soluciones.pera@gmail.com>
2026-02-22 19:13:10 -0500

    fix: Directory path into build step file

--- build_step.sh
+++ build_step.sh
@@ -4,4 +4,4 @@ echo "Build script"
 
 # add the commands here
 cd backend && rm -rf dist && cd ../frontend && npm install && npm run build && cp -r dist ../backend
-cd backend && npm install && npm run build
+cd ../backend && npm install && npm run build
